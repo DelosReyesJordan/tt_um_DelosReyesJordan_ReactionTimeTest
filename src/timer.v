5509fba56e207814db8710012348f5e64997d9fa
DelosReyesJordan <63662211+DelosReyesJordan@users.noreply.github.com>
2025-06-28 00:14:12 -0700

    Update timer.v

--- src/timer.v
+++ src/timer.v
@@ -1,13 +1,13 @@
 module timer(
     input clk, reset, start, stop,
-    output reg [13:0] ms_time // 0-9999 ms
+    output reg [13:0] ms_time
 );
-    reg [16:0] count_100us;
+    reg [14:0] cycle_count;
     reg counting;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            count_100us <= 0;
+            cycle_count <= 0;
             ms_time <= 0;
             counting <= 0;
         end else begin
@@ -17,12 +17,10 @@ module timer(
                 counting <= 0;
 
             if (counting) begin
-                count_100us <= count_100us + 1;
-
-                if (count_100us == 99999) begin
-                    count_100us <= 0;
-                    if (ms_time < 9999)
-                        ms_time <= ms_time + 1;
+                cycle_count <= cycle_count + 1;
+                if (cycle_count == 25000) begin
+                    cycle_count <= 0;
+                    ms_time <= ms_time + 1;
                 end
             end
         end
